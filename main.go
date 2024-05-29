4d4f06d8924db230b46dbb1055bb3a3c1cac1261
Alex Gwartney <gwartneydev@gmail.com>
2024-05-28 18:11:15 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,108 +1,76 @@
 package main
 
-import (
-	"fmt"
-)
-
-// Define the Node structure
 type Node struct {
-	data int
+	val  int
 	next *Node
 }
 
-// Define the LinkedList structure
-type LinkedList struct {
+type LinkList struct {
 	head *Node
+	size int
 }
 
-// Initialize a new empty linked list
-func NewLinkedList() *LinkedList {
-	return &LinkedList{}
-}
+func (l *LinkList) eeque(val int) {
 
-// Add a new node at the end of the linked list
-func (ll *LinkedList) Append(data int) {
-	newNode := &Node{data: data, next: nil}
-
-	if ll.head == nil {
-		ll.head = newNode
-		return
-	}
-
-	current := ll.head
-	for current.next != nil {
-		current = current.next
-	}
-	current.next = newNode
-}
-
-// Display the linked list
-func (ll *LinkedList) Display() {
-	current := ll.head
-	for current != nil {
-		fmt.Printf("%d -> ", current.data)
-		current = current.next
-	}
-	fmt.Println("nil")
-}
-
-func main() {
-	// Create a new linked list
-	ll := NewLinkedList()
-
-	// Add elements to the linked list
-	ll.Append(10)
-	ll.Append(20)
-	ll.Append(30)
-
-	// Display the linked list
-	fmt.Println("Linked List:")
-	ll.Display()
-}
-
-
-///UPDATED 
-
-package main
-
-type Node struct {
-	value int
-	next  *Node
-}
-
-type linkedList struct {
-	head *Node
-}
-
-func (l *linkedList) add(value int) {
-	newNode := &Node{value: value} // Create a new node with the given value
-
+	newNode := &Node{val: val, next: nil}
 	if l.head == nil {
-		// If the list is empty, set the new node as the head
 		l.head = newNode
 	} else {
-		// Otherwise, traverse the list to find the last node
 		current := l.head
 		for current.next != nil {
-			current = current.next // Move to the next node
+			current = current.next
 		}
-		// After the loop, current points to the last node in the list
-		current.next = newNode // Link the new node to the end of the list
+		current.next = newNode
+
+		l.size++
+
 	}
 }
 
-// adds to the front of a linkd list
-func (l *linkedList) push(value int) {
+func (l *LinkList) dnque() int {
+	if l.head == nil {
+		return 0
+	}
+	temp := l.head.val
+	l.head = l.head.next
+	l.size--
+	return temp
+}
 
-	newNode := &Node{value: value}
-
+func (l *LinkList) push(val int) int {
+	newNode := &Node{val: val, next: nil}
 	if l.head == nil {
 		l.head = newNode
-
 	} else {
 		temp := l.head
 		l.head = newNode
 		l.head.next = temp
 	}
+	l.size++
+
+	return l.size
 }
 
+func (l *LinkList) pop() int {
+	if l.head == nil {
+		return 0 // or another sentinel value to indicate the stack is empty
+	}
+
+	temp := l.head.val
+	l.head = l.head.next
+	l.size--
+	return temp
+}
+
+func (l *LinkList) revdrse(val int) {
+	var prev *Node
+	var next *Node
+	current := l.head //2
+	for current.next != nil {
+		next = current.next //3
+		current.next = prev //nil
+		prev = current      // prev = 2
+		current = next      // current becomes
+	}
+	l.head = prev // set the node to the first node
+}
