1cc9db0ba4623de8e10000467c743d61475faa53
Alex Gwartney <gwartneydev@gmail.com>
2024-05-28 18:10:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ class LinkList {
         this.head = null;
     }
 }
- // Initialize nodes
+
+
+
+
 let node1 = new Node(1);
 let node2 = new Node(2);
 let node3 = new Node(3);
@@ -22,43 +25,65 @@ let node3 = new Node(3);
 node1.next = node2;
 node2.next = node3;
 
-// Create linked list and set the head
-let linkedList = new  LinkList();
-linkedList.head = node1;
-
 //previous = null null->currentNode1
 function reverses(node){
     
-    //null<- node1->next->node2->node3->null
+    //tail<- node1->next->node2->node3->head
     //previous 
     //current 
     //next
 
-    let current = node;
-    let prev = null;
-  
-    while(current !=null){
-       let temp = current.next; 
-       current.next = prev; 
-       prev = current;
-       current = temp;
-    
+    while (current != null) {
+        next = current.next; // Store the next node
+        current.next = prev; // Reverse the current node's pointer
+        prev = current;      // Move prev to current node
+        current = next;      // Move to the next node
     }
-    
-    return prev;
-  
+
+    return prev; // prev is the new head of the reversed list
 }
 
-linkedList.head = reverses(linkedList.head);
- 
-// Function to display the list
-function displayList(head) {
-    let current = head;
-    while (current !== null) {
-        console.log(current.data);
-        current = current.next;
+
+reverses(current)
+
+
+class ArrayClass {
+    constructor() {
+        this.length = 0;
+    }
+
+    // Method to add an element to the end of the array
+    push(element) {
+        this[this.length] = element;
+        this.length++;
+        return this.length;
+    }
+
+    // Method to add an element to the beginning of the array
+    unshift(element) {
+        // Shift elements to the right
+        for (let i = this.length; i > 0; i--) {
+            this[i] = this[i - 1];
+        }
+        // Place the new element at the start
+        this[0] = element;
+        // Increment the length
+        this.length++;
+        return this.length;
+    }
+
+    // Method to get an element by index
+    get(index) {
+        return this[index];
     }
 }
 
-displayList(linkedList.head)
+// Example usage
+let myArray = new ArrayClass();
+myArray.push(1);
+myArray.push(2);
+myArray.push(3);
+console.log(myArray); // Outputs: ArrayClass { '0': 1, '1': 2, '2': 3, length: 3 }
 
+myArray.unshift(4);
+console.log(myArray); // Outputs: ArrayClass { '0': 4, '1': 1, '2': 2, '3': 3, length: 4 }
